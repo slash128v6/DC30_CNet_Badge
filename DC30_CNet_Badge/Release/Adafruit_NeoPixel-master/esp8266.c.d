04dfb8697b80fdf0d36d6992eba986ad01e0d0d5
slash128 <jg@slash128.com>
2022-04-25 15:49:11 -0700

    Add files via upload

---
+++ DC30_CNet_Badge/Release/Adafruit_NeoPixel-master/esp8266.c.d
@@ -0,0 +1,2 @@
+C:\Users\jgranat\AppData\Local\Temp\VMBuilds\DC30_CNet_Badge\SparkFun_promicro_16MHzatmega32U4\Release\Adafruit_NeoPixel-master\esp8266.c.o: \
+ C:\Users\jgranat\My\ Drive\Dev\Arduino\libraries\Adafruit_NeoPixel-master\esp8266.c
---
+++ DC30_CNet_Badge/Release/DC30_CNet_Badge.cpp.d
@@ -0,0 +1,17 @@
+C:\Users\jgranat\AppData\Local\Temp\VMBuilds\DC30_CNet_Badge\SparkFun_promicro_16MHzatmega32U4\Release\DC30_CNet_Badge.cpp.o: \
+ C:\Users\jgranat\AppData\Local\Temp\VMBuilds\DC30_CNet_Badge\SparkFun_promicro_16MHzatmega32U4\Release\DC30_CNet_Badge.cpp \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBDesc.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBCore.h \
+ C:\Users\jgranat\AppData\Local\arduino15\packages\SparkFun\hardware\avr\1.1.13\variants\promicro/pins_arduino.h \
+ C:\Users\jgranat\My\ Drive\Dev\Arduino\libraries\Adafruit_NeoPixel-master/Adafruit_NeoPixel.h \
+ C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h
